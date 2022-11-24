8702a9e5b63e806e4aa1b460795649ed14e5b564
mondaymood <84736910+mondaymood@users.noreply.github.com>
2022-11-24 12:15:29 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './_includes/**/*.{html,md}',
+    './_layouts/**/*.{html,md}',
+    './_*/**/*.{html,md}',
+    '!./_site/**/*.{html,md}',
+    './*.{html,md}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
