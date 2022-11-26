6f86b2283347fabda88f8f9b504d81ad81f2ec76
mondaymood <hello@taraotto.com>
2022-11-26 16:35:48 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
   theme: {
     extend: {
     },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     listStyleType: {
       none: 'none',
       disc: 'disc',
